6c726e8d6e94b66d88ab1b8992040e56f128a217
rogcomfox <joeg.11021996@gmail.com>
2020-09-12 13:44:38 +0700

    add tab layout

--- app/src/main/java/com/nusantarian/ngelabpremiumchat/activity/MainActivity.kt
+++ app/src/main/java/com/nusantarian/ngelabpremiumchat/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.nusantarian.ngelabpremiumchat.activity
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,6 +15,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.nusantarian.ngelabpremiumchat.R
 import com.nusantarian.ngelabpremiumchat.databinding.ActivityMainBinding
 import com.nusantarian.ngelabpremiumchat.fragment.HomeFragment
@@ -28,23 +31,33 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab_chat)
+        val fab: FloatingActionButton = findViewById(R.id.fab_new_message)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        initMainFragment()
-    }
-
-    private fun initMainFragment(){
-        supportFragmentManager.beginTransaction()
-            .add(R.id.frame_main, HomeFragment())
-            .commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_search ->
+                Toast.makeText(applicationContext, "Search, Under Construction", Toast.LENGTH_LONG).show()
+            R.id.action_settings ->
+                Toast.makeText(applicationContext, "Settings, Under Construction", Toast.LENGTH_LONG).show()
+            R.id.action_sign_out ->
+                raiseSignOutDialog()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun raiseSignOutDialog(){
+        val builder = MaterialAlertDialogBuilder(this)
+        builder.setMessage(R.string.message_sign_out)
     }
 }
